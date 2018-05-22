9dd6d390d62823190992471132ca6d8b594666cc
sergey.sedelnikov <sergey.sedelnikov@storaenso.com>
2018-05-22 06:57:36 +0300

    sample mqtt job

---
+++ jobs/sample-mqtt-job.js
@@ -0,0 +1,35 @@
+const { MqttMessageJob } = require('../src/base/mqtt-job');
+
+/**
+ * Sample job that runs on MQTT event.
+ */
+class SampleMqttEventJob extends MqttMessageJob {
+
+    /** Gets the MQTT broker address */
+    getMqttBrokerUri(){
+        // return broker address string.
+        return 'mqtt.intelligentpackaging.online';
+    }
+    
+    /** Returns the array of topics to subscribe to. */
+    getListOfTopics(){
+        return [
+            'topic1',
+            'topic2'
+        ]
+    }
+
+    /**
+     * Executes on MQTT message received.
+     * @param {String} topic Topic that fired the message.
+     * @param {Buffer} message Message buffer.
+     */
+    onMessage(topic, message){
+        let strMessage = message.toString();
+        console.log(`Mqtt message received: ${topic}, ${strMessage}`);
+        // update widgets
+        this.sendEvent(topic, strMessage);
+    }
+}
+
+module.exports = SampleMqttEventJob;
--- src/base/mqtt-job.js
+++ src/base/mqtt-job.js
@@ -4,7 +4,7 @@ const { Job } = require('./job');
 /** Runs job on schedule. */
 class MqttMessageJob extends Job {
 
-    /** Gets the address of the MQTT broker to connect to. */
+    /** Gets the address of the MQTT broker to connect to. For instance: mqtt://test.mosquitto.org*/
     getMqttBrokerUri() {
         throw 'The method needs to return MQTT broker URI, for instance: mqtt://test.mosquitto.org';
     }
@@ -16,8 +16,8 @@ class MqttMessageJob extends Job {
 
     /**
      * Executes on MQTT message arrived
-     * @param {*} topic Received topic
-     * @param {*} message Message buffer
+     * @param {String} topic Received topic
+     * @param {Buffer} message Message buffer
      */
     onMessage(topic, message){
         throw 'Method onMessage(topic, message) need to be implemented. The method fires on MQTT message received.'
@@ -31,6 +31,7 @@ class MqttMessageJob extends Job {
         const client = mqtt.connect(brokerAddress);
 
         client.on('connect', function () {
+            console.log(`Job ${this.id} is connected to MQTT ${brokerAddress}. Waiting for messages...`);
             // on connected, subscribe for topics
             let topics = getListOfTopics();
             topics.forEach(topic => {
