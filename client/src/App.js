048c47c0cbf4bc1a2965724c1a1fd254444a2b34
Nostalginen <joni.mettovaara@bitfactor.fi>
2018-05-22 13:44:50 +0300

    Add React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import * as Dashboards from './Dashboards'
 import './App.css';
 import 'react-grid-layout/css/styles.css';
@@ -7,7 +8,12 @@ import 'react-resizable/css/styles.css';
 class App extends Component {
   render() {
     return (
-      <Dashboards.Dashboard_1></Dashboards.Dashboard_1>
+      <Router>
+        <div>
+          <Route exact path="/"  render={() => (<Redirect to="/dashboard_1"/>)} />
+          <Route exact path="/dashboard_1" component={Dashboards.Dashboard_1} />
+        </div>
+      </Router>
     );
   }
 }
