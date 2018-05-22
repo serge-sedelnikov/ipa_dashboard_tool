4752d3a59c43c600c582106b8bdad733618a2af4
sergey.sedelnikov <sergey.sedelnikov@storaenso.com>
2018-05-22 06:43:34 +0300

    mqtt job code, abstract methods.

--- src/base/mqtt-job.js
+++ src/base/mqtt-job.js
@@ -1,13 +1,29 @@
+const mqtt = require('mqtt');
 const { Job } = require('./job');
 
 /** Runs job on schedule. */
 class MqttMessageJob extends Job {
 
-    /** Connects to given MQTT broket, subscribes for topics and waits for messages. */
+    /** Gets the address of the MQTT broker to connect to. */
+    getMqttBrokerUri(){
+        throw 'The method needs to return MQTT broker URI, for instance: mqtt.mosquitto.com';
+    }
+
+    /** Gets the array of topics to subscribe to. */
+    getListOfTopics(){
+        throw 'The method needs to return the array of strings: topics to subscribe to. For instance: ["topic1/#", "topic2", "topic3/event1/value3"]';
+    }
+
+    /** Connects to MQTT */
+    initializeAsync(){
+        return Promise((res, rej)=> {
+
+        });
+    }
+
+    /** Connects to given MQTT broker, subscribes for topics and waits for messages. */
     run(){
-        // get cron schedule
-        const cron = this.getSchedule();
-        schedule.scheduleJob(cron, this.onSchedule.bind(this));
+        // subscribe for topics
     }
 }
 
