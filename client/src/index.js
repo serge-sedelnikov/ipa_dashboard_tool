c40d12514130f4c89db69ba18f7eae92b868d212
Sedelnikov <sergey.sedelnikov@storaenso.com>
2018-11-12 19:46:31 +0200

    custom css theme added (Stora Enso)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// base bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
+// custom styles css
+import '@storaensods/se-design-system/dist/scss/index.scss';
+
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import './index.css';
