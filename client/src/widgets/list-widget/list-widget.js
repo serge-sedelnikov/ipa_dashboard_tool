cdd4c782df656a72d00c95f9194d5567d658ae2a
Sedelnikov <sergey.sedelnikov@storaenso.com>
2018-11-12 20:53:00 +0200

    list widget supports icons

--- client/src/widgets/list-widget/list-widget.js
+++ client/src/widgets/list-widget/list-widget.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Card } from 'reactstrap';
 import Widget from '../base-widget';
 import './list-widget.css';
 
-export default class ListWidget extends Widget{    
-    render(){
-        if(this.state.response){
-            return(
+export default class ListWidget extends Widget {
+    render() {
+        if (this.state.response) {
+            return (
                 <ListGroup>
-                {this.state.response.map((o, i) => {
-                    return(
-                        <ListGroupItem key={i}>{o}</ListGroupItem>
-                    )
-                })}
+                    {this.state.response.map((o, i) => {
+                        return (
+                            <ListGroupItem key={i}>
+                                <div className="media">
+                                    {o.icon ? <img className="mr-3" src=".../64x64" alt="Generic placeholder image" /> : null}
+                                    <div className="media-body">
+                                        {o.text || o}
+                                    </div>
+                                </div>
+                            </ListGroupItem>
+                        )
+                    })}
                 </ListGroup>
             )
-        }else{
-            return(
-                null
+        } else {
+            return (
+                <Card className="h-100"></Card>
             )
         }
     }
