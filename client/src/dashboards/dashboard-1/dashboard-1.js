c1ac5f4db8f2cb48b8a62d5681bd7231c22ac902
Sedelnikov <sergey.sedelnikov@storaenso.com>
2018-11-13 23:27:13 +0200

    param set to be fired from widget to job

--- client/src/dashboards/dashboard-1/dashboard-1.js
+++ client/src/dashboards/dashboard-1/dashboard-1.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Card } from 'reactstrap';
+import { Input, Button } from '@storaensods/se-design-system';
 
 import Dashboard from '../base-dashboard';
 import { NumberWidget, TextWidget, ListWidget } from '../../widgets';
+import Widget from '../../widgets/base-widget';
 import './dashboard-1.css';
+
+/**
+ * Parameters edit form.
+ */
+export class NameParamForm extends Widget {
+  
+  constructor(){
+    super();
+    this.state = {
+      ...this.state,
+      newName: ''
+    }
+  }
+
+  /**
+   * Passes the argument to the backend.
+   */
+  handleNameSubmit(){
+    this.updateParameterValue('userName', this.state.newName);
+  }
+
+  /**
+   * On user enters value.
+   * @param {*} event Change event
+   */
+  handleNameChange(event){
+    console.log(event.target.value);
+    this.setState({
+      ...this.state,
+      newName: event.target.value
+    })
+  }
+
+  /**
+   * Renders.
+   */
+  render() {
+    return (<Card body>
+      <form>
+        <div className="form-group">
+          <label htmlFor="nameInput">Enter your name here</label>
+          <Input id="nameInput" placeholder="your name here..." value={this.state.newName} onChange={this.handleNameChange.bind(this)}/>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your name with anyone else.</small>
+        </div>
+        <Button icon="done" size="sm" onClick={this.handleNameSubmit.bind(this)}>Submit</Button>
+      </form>
+    </Card>)
+  }
+}
+
 
 export default class Dashboard1 extends Dashboard {
 
@@ -23,7 +75,7 @@ export default class Dashboard1 extends Dashboard {
           </Col>
         </Row>
 
-        
+
         <Row>
           {/* Generic numbers */}
           <Col lg="2" md="4" xs="12">
@@ -33,7 +85,14 @@ export default class Dashboard1 extends Dashboard {
             <NumberWidget dataId="random-number" value="count" header="Total count" footer="of visitors" suffix="%" />
           </Col>
           <Col lg="4" md="4" xs="12">
-            <TextWidget dataId="schedule-matched" header="Ticks" footer="Last time refreshed" />
+            <Row className="h-100">
+              <Col lg="12">
+                <TextWidget dataId="schedule-matched" header="Ticks" footer="Last time refreshed" />
+              </Col>
+              <Col lg="12" className="mt-1">
+                <TextWidget dataId="parameterized-data" header="I am reacting on parameter" footer="Edit it below, job will accept it and will refresh the value" />
+              </Col>
+            </Row>
           </Col>
           <Col lg="2" md="4" xs="12">
             <NumberWidget dataId="topic1" header="MQTT Number 1"></NumberWidget>
@@ -46,6 +105,14 @@ export default class Dashboard1 extends Dashboard {
           </Col>
           {/* / List */}
         </Row>
+
+        {/* Edit parameter */}
+        <Row className="mt-3">
+          <Col lg="5" md="6" xs="12">
+            <NameParamForm />
+          </Col>
+        </Row>
+        {/* / Edit parameter */}
       </div>
     )
   }
